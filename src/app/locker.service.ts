af64a79e2258f73cf06cfc389e047b473bcc0af8
Alicja Kawecka <alicemiriel@gmail.com>
2023-08-27 18:01:27 +0200

    Restore Locker Tiles

--- src/app/locker.service.ts
+++ src/app/locker.service.ts
@@ -11,23 +11,9 @@ export class LockerService {
 
   getLockers(): Observable<Locker[]> {
     return this.http.get<Locker[]>('api/lockers');
-    // .pipe(catchError(this.handleError<Locker[]>('getLockers', [])));
   }
 
   updateLocker(locker: Locker): Observable<any> {
     return this.http.put(`api/lockers/${locker.id}`, locker);
   }
-
-  // private handleError<T>(operation = 'operation', result?: T) {
-  //   return (error: any): Observable<T> => {
-  //     // TODO: send the error to remote logging infrastructure
-  //     console.error(error); // log to console instead
-  //
-  //     // TODO: better job of transforming error for user consumption
-  //     this.log(`${operation} failed: ${error.message}`);
-  //
-  //     // Let the app keep running by returning an empty result.
-  //     return of(result as T);
-  //   };
-  // }
 }
