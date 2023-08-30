2a360b06c351f475fb0775be7d364876bd9d46e8
Alicja Kawecka <alicemiriel@gmail.com>
2023-08-30 15:19:23 +0200

    Added storybook and first stories

---
+++ src/app/locker-page/locker-list/locker-list.stories.ts
@@ -0,0 +1,58 @@
+import type { Meta } from '@storybook/angular';
+import { moduleMetadata, StoryObj } from '@storybook/angular';
+import { LockerListComponent } from './locker-list.component';
+import { LockerStatus } from '../../locker.type';
+import { LockerTileComponent } from './locker-tile/locker-tile.component';
+import { LockerNumberPipe } from './locker-tile/locker-number.pipe';
+import { LockerStatusPipe } from './locker-tile/locker-status.pipe';
+
+interface StoryProps {
+  lockers: { number: number; status: LockerStatus }[];
+}
+
+const meta: Meta<StoryProps> = {
+  title: 'Locker List',
+  component: LockerListComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [
+        LockerListComponent,
+        LockerTileComponent,
+        LockerNumberPipe,
+        LockerStatusPipe,
+      ],
+    }),
+  ],
+  render: (args: StoryProps) => ({
+    props: {
+      lockers: [...args.lockers],
+    },
+  }),
+  args: {
+    lockers: [],
+  },
+  argTypes: {
+    lockers: {
+      control: 'array',
+      table: {
+        type: {
+          number: 'number',
+          status: 'text',
+        },
+      },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<StoryProps>;
+
+export const Primary: Story = {
+  args: {
+    lockers: [
+      { number: 1, status: LockerStatus.BUSY },
+      { number: 2, status: LockerStatus.FREE },
+      { number: 3, status: LockerStatus.RESERVED },
+    ],
+  },
+};
---
+++ src/app/locker-page/locker-list/locker-tile/locker-tile.stories.ts
@@ -0,0 +1,55 @@
+import type { Meta } from '@storybook/angular';
+import { moduleMetadata, StoryObj } from '@storybook/angular';
+import { LockerTileComponent } from './locker-tile.component';
+import { LockerStatus } from '../../../locker.type';
+import { LockerNumberPipe } from './locker-number.pipe';
+import { LockerStatusPipe } from './locker-status.pipe';
+
+const meta: Meta<{ number: number; status: LockerStatus }> = {
+  title: 'Locker Tile',
+  component: LockerTileComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [LockerTileComponent, LockerNumberPipe, LockerStatusPipe],
+    }),
+  ],
+  render: (args: { number: number; status: LockerStatus }) => ({
+    props: {
+      locker: {
+        ...args,
+      },
+    },
+  }),
+  argTypes: {
+    number: { control: 'number' },
+    status: {
+      control: 'select',
+      options: [LockerStatus.FREE, LockerStatus.BUSY, LockerStatus.RESERVED],
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<{ number: number; status: LockerStatus }>;
+
+export const Primary: Story = {
+  name: 'Reserved',
+  args: {
+    number: 123,
+    status: LockerStatus.RESERVED,
+  },
+};
+
+export const Free: Story = {
+  args: {
+    number: 123,
+    status: LockerStatus.FREE,
+  },
+};
+
+export const Busy: Story = {
+  args: {
+    number: 123,
+    status: LockerStatus.BUSY,
+  },
+};
